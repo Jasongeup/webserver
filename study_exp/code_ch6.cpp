0f2ecc791fb25316c41eb370588eb7bd0e2b7265
Jasongeup <3396822177@qq.com>
2025-03-09 16:14:13 +0800

    pid learned

--- study_exp/code_ch6.cpp
+++ study_exp/code_ch6.cpp
@@ -240,7 +240,7 @@ int main(){
     cout << "userid is" << uid << ", effective userid is" << euid << endl;
     return 0;
 }*/
-//切换用户，将以root身份启动的进程切换为以一个普通用户身份运行
+/*切换用户，将以root身份启动的进程切换为以一个普通用户身份运行
 static bool switch_to_user(uid_t user_id, gid_t gp_id){
     if((user_id == 0) && (gp_id == 0)) return false;    // 先确保目标用户不是root
     gid_t gid = getgid();
@@ -250,5 +250,30 @@ static bool switch_to_user(uid_t user_id, gid_t gp_id){
     if(uid != 0) return true;     // 如果不是root，则已经是目标用户
     // 切换到目标用户
     if((setgid(gp_id) < 0) || (setuid(user_id) < 0)) return false;
+    return true;
+}*/
+//将服务器程序以守护进程的方式进行
+bool daemonize(){
+    // 创建子进程，关闭父进程，这样可以使程序在后台运行
+    pid_t pid = fork();
+    if(pid < 0) return false;
+    if(pid > 0) exit(0);
+    // 设置文件权限掩码。当进程创建新文件（使用open(const char*pathname,int flags,mode_t mode)系统调用）时，文件的权限将是mode＆0777
+    umask(0);
+    // 创建新的会话，设置本进程为进程组的首领
+    pid_t sid = setsid();
+    if(sid < 0) return false;
+    //切换工作目录
+    if((chdir("/")) < 0) return false;
+    // 关闭标准输入设备、标准输出设备和标准错误输出设备
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+    // 关闭其他已经打开的文件描述符
+    // close(fd);
+    // 将标准输入、标准输出和标准错误输出都定向到/dev/null文件
+    open("/dev/null", O_RDONLY);
+    open("/dev/null", O_RDWR);
+    open("/dev/null", O_RDWR);
     return true;
 }
