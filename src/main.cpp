c8065061330cb160399db2b08b906f4ecf86e804
Jasongeup <3396822177@qq.com>
2025-03-31 22:47:42 +0800

    docs: add main.cpp and resources

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,22 @@
-#include<iostream>
+/***********************************************
+ * FileName    : main.cpp
+ * Description : run webserver
+ * 
+ * Feature     :
+ * 
+ * Author      : JasonGe
+ * Created on  : 2025/03/30
+************************************************/
+#include <unistd.h>
+#include <iostream>
+#include <libgen.h>
+#include "server/webserver.h"
+
 int main() {
-    int x = 3;
-    int&& y = std::move(x);
-    y = 5;
-    std::cout << x;
-    return 0;
-}
+    WebServer server(
+        1316, 3, 60000, false,             /* 端口 ET模式 timeoutMs 优雅退出  */
+        3306, "root", "root", "webserver", /* Mysql配置 */
+        12, 6, true, 1, 1024);             /* 连接池数量 线程池数量 日志开关 日志等级 日志异步队列容量 */
+    server.Start();
+} 
+  
